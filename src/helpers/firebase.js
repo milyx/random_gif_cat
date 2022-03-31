defc9883c7eacba61e1b7dc6460546caf43ac2b1
milagros acosta <milyarias91@gmail.com>
2022-03-31 11:07:55 -0300

    project creation

---
+++ src/helpers/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD-soh5_qbyKMnD8B9vsI96c94oB4UdT8",
+  authDomain: "prueba-c320d.firebaseapp.com",
+  projectId: "prueba-c320d",
+  storageBucket: "prueba-c320d.appspot.com",
+  messagingSenderId: "504613414985",
+  appId: "1:504613414985:web:5122ec88656da86bd7b798"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
